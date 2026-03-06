328767e13693871681841fa50719a30d6629df5e
Nashely Turizo <nturizov@ufpso.edu.co>
2026-03-06 15:43:13 -0500

    Ejercicio Practico

---
+++ src/ejercicio_practico.java
@@ -0,0 +1,26 @@
+public class ejercicio_practico {
+    public static void main(String[] args) {
+        
+        // Declaracion y asignacion
+        int edad = 20;
+        System.out.println("Edad: " + edad);
+        double altura = 1.65;
+        System.out.println("Altura: " + altura);
+        char inicial = 'N';
+        System.out.println("Inicial del nombre: " + inicial);
+        boolean esMayorDeEdad = true;
+        System.out.println("¿Es mayor de edad?: " + esMayorDeEdad);
+
+        // Declaracion y asignacion
+        int edad2 = 15;
+        System.out.println("Edad: " + edad2);
+        double altura2 = 1.65;
+        System.out.println("Altura: " + altura2);
+        char inicial2 = 'C';
+        System.out.println("Inicial del nombre: " + inicial2);
+        boolean esMayorDeEdad2 = false;
+        System.out.println("¿Es mayor de edad?: " + esMayorDeEdad2);
+    }    
+
+     
+}
