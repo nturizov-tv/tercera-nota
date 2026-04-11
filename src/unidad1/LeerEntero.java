c55540f31e62373325766652f279e9093fe002d9
Nashely Turizo <nturizov@ufpso.edu.co>
2026-04-10 23:11:24 -0500

    Unidad 1

---
+++ src/unidad1/LeerEntero.java
@@ -0,0 +1,14 @@
+package unidad1;
+import java.util.Scanner;
+
+public class LeerEntero {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingresa un número entero: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Número entero leído: " + numero);
+        scanner.close();
+    }
+}
